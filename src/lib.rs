036627bcfaa6ad5714ea99ca8ee28f04f114df17
Colin Ray <r.colinray@gmail.com>
2015-06-16 10:46:56 -0500

    Improve documentation through lib source

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,28 @@
 #![feature(collections)]
+
+//! A TIS-100 emulator.
+//!
+//! Examples:
+//!
+//! ```
+//! use tis_100::save::parse_save;
+//! use tis_100::machine::Sandbox;
+//!
+//! // This program reads the value from the console and simply passes it to the console output.
+//! let src = "@1\nMOV UP DOWN\n@5\nMOV UP DOWN\n@9\nMOV UP RIGHT\n@10\nMOV LEFT DOWN\n";
+//!
+//! let save = parse_save(src).unwrap();
+//! let mut sandbox = Sandbox::with_save(&save);
+//!
+//! sandbox.write_console(42);
+//!
+//! for _ in 0..5 {
+//!     sandbox.step();
+//! }
+//!
+//! assert_eq!(sandbox.read_console(), Some(42));
+//! ```
+
 pub mod core;
 pub mod lex;
 pub mod parse;
